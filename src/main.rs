22858b6a4da955e3e40cd75f25e784494bcb0fd9
Christos Dalgaroglou <christos.daglaroglou@gmail.com>
2026-09-07 15:45:08 +0300

    Basic arguments implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,51 @@
+use clap::{Parser, Subcommand};
+
 mod cli;
 mod clipboard;
 mod storage;
 mod totp;
 mod ui;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Get {
+        name: String
+    },
+
+    Add {
+        name: String,
+        secret: String
+    },
+
+    Rm {
+        name: String
+    },
+
+    List,
+}
+
 fn main() {
-    println!("OTtyP");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Get { name } => {
+            println!("Fetching code for: {}", name);
+        }
+        Commands::Add { name, secret } => {
+            println!("Adding secret for: {} (secret: {})", name, secret);
+        }
+        Commands::Rm { name } => {
+            println!("Removing account: {}", name);
+        }
+        Commands::List => {
+            println!("Listing all accounts...");
+        }
+    }
 }
